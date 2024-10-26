0e319543cbe2a270f109ecb3365c71141db6a87d
Wang Linhao <wilkinsddvd@qfnu.edu.cn>
2024-10-26 21:35:55 +0800

    django 项目 10.26晚 更新

--- web_project/web_project/urls.py
+++ web_project/web_project/urls.py
@@ -1,14 +1,18 @@
 # from django.conf.urls import url 已废弃
 from django.urls import re_path as url
 from . import views, testdb, search, search2
+from django.contrib import admin
 
 urlpatterns = [
     # url(r'^hello/$', views.hello),
+    url(r'^runoob/$', views.runoob),
     url(r'^testdb/$', testdb.testdb),
     url(r'^search-form/$', search.search_form),
     url(r'^search/$', search.search),
     url(r'^search-post/$', search2.search_post),
+    url(r'^admin/', admin.site.urls), # 管理后台
 ]
+
 # from django.conf.urls import url  已废弃
 # from django.urls import re_path as url
 # from . import views, testdb, search
